fcc1e466d3beaf6a157d6e189bd4dae2d45ee5d8
ahmet Durukan <drknahmet51@gmail.com>
2021-01-30 16:23:06 +0300

    Add project files.

---
+++ PcCekilisi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PcCekilisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------KATILIMCILAR-----------");
+
+            string[] Isım = new string[] { "Burak Kamber", "Ali Can", "Serdar Doğan", "Arif Elmas", "Kemal Cansever", "Yaşar Gül","Eren Güçlü" };
+
+            for (int i = 0; i < Isım.Length; i++)
+            {
+                Console.WriteLine(i + 1 + ". Katılımcı " + "Adı-Soyadı: " + Isım[i]);
+
+            }
+
+            Console.WriteLine("Toplam Katılımcı Sayısı: " + Isım.Length);
+        }
+    }
+}
